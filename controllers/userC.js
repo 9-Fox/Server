4531156390887b204e53591bc1e80f473086cc8b
alvihrhp <rafael.hrhp@yahoo.com>
2020-01-16 20:59:29 +0700

    Add register feautures

--- controllers/userC.js
+++ controllers/userC.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const User = require('../models/user')
+const User = require('../models/user');
+const Bcrypt = require('../helpers/bcrypt');
+const Jwt = require('../helpers/jwt')
 
 class Controller {
     static register(req, res, next) {
@@ -16,6 +18,33 @@ class Controller {
                 Message: "New user has been created",
                 data: user
             })
+        })
+        .catch(error => {
+            next(error);
+        })
+    }
+
+    static login(req, res, next) {
+        User.findOne({email: req.body.email})
+        .then(user => {
+            if(!user) {
+                throw {
+                    name: 'NotFound'
+                }
+            } else {
+                const validationPassword = Bcrypt.dehash(req.body.password, user.password);
+                if(!validationPassword) {
+                    throw {
+                        name: "ValidationError"
+                    }
+                } else {
+                    const userToken = Jwt.genToken(user._id);
+                    res.status(200).json({
+                        Message: 'OK',
+                        data: userToken
+                    })
+                }
+            }
         })
         .catch(error => {
             next(error);
